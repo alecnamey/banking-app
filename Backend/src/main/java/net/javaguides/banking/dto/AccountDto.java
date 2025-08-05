e344dbec1a823178f42cf0eb8fd2873883f4eb5f
Alec Namey <144381448+alecnamey@users.noreply.github.com>
2025-08-05 12:36:51 -0700

    backend completed

---
+++ Backend/src/main/java/net/javaguides/banking/dto/AccountDto.java
@@ -0,0 +1,15 @@
+package net.javaguides.banking.dto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+public class AccountDto {
+    private Long id;
+    private String accountHolderName;
+    private double balance;
+}
